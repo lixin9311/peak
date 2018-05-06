a751ebfcf57ff0160bdc6f3713b58b3bf5af1844
Lucus <lixin9311@gmail.com>
2018-05-06 23:50:49 +0900

    data generate script

---
+++ gen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PROG=${PROG:-./a.out}
+FROM=${FROM:-800}
+TO=${TO:-$(($FROM*128))}
+MULT=${MULT:-2}
+REPEAT=${REPEAT:-10}
+N=${N:-$((8*14*4))}
+
+>&2 echo "Program: $PROG"
+>&2 echo "Range: $FROM ~ $TO, mult $MULT"
+>&2 echo "Repeat: $REPEAT"
+>&2 echo "N: $N"
+
+for ((i=$FROM; i<=$TO; i*=$MULT)); do
+    echo "Size $((N * i))" >&2
+    echo -ne "$((N * i))\t"
+    for ((j=0; j < $REPEAT; j++)); do
+        echo -ne "$($PROG $i)\t"
+    done
+    echo ""
+done
